8d349ed505f9b2bf3299aa63bbec603b7c99be92
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-06-22 11:25:11 -0400

    add code for unweighted undirected graph

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -1,7 +1,45 @@
-class Vertex {
-  constructor() {}
-}
-
+// undirected, unweighted graph
 class Graph {
-  constructor() {}
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    // accepts the name of a vertex
+    // should add a key to the list with the name of the vertex and set its value to be an empty array
+    // duplicates will be ignored (can add extra handling)
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(v1, v2) {
+    // should accept two vertices
+    // find in the adjacencyList the key of v1 and push v2 to array
+    // find in the adjacencyList the key of v2 and push v1 to array
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+
+  removeEdge(v1, v2) {
+    // reassign the key of v1 to be an array that does not contain v2
+    // reassign the key of v2 to be an array that does not contain v1
+    this.adjacencyList[v1].filter((v) => {
+      return v !== v2;
+    });
+
+    this.adjacencyList[v2].filter((v) => {
+      return v !== v1;
+    });
+  }
+
+  removeVertex(vertex) {
+    // remove all edges and the vertex itself
+    // loop as long as there are any other vertices in the adjacency list for that vertex
+    // inside the loop, call removeEdge function with the vertex we are removing and any values in the adjacencyList for that vertex
+    // delete the key in the adjacencyList for that vertex
+    while (this.adjacencyList[vertex].length) {
+      const adjVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
 }
