0cce2818cde28d5fe6ee04e18365de9d40a1f74c
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-08-24 12:07:27 -0400

    solved: merge ranges

---
+++ PRACTICE/interview-cake/mergeRanges.test.js
@@ -0,0 +1,39 @@
+// Write a function mergeRanges() that takes an array of multiple meeting time ranges and returns an array of condensed ranges.
+// BIG O: time O(n logn) / space O(n)
+
+const mergeRanges = (meetings) => {
+  const copy = JSON.parse(JSON.stringify(meetings));
+
+  let sorted = copy.sort((a, b) => a.startTime - b.startTime);
+  let result = [sorted[0]];
+
+  for (let i = 0; i < sorted.length; i++) {
+    const current = sorted[i];
+    const temp = result[result.length - 1];
+
+    if (current.startTime <= temp.endTime) {
+      temp.endTime = Math.max(temp.endTime, current.endTime);
+    } else {
+      result.push(current);
+    }
+  }
+  return result;
+};
+
+describe("merges an array of multiple meeting times into condensed ranges", () => {
+  it("returns a condensed range of the input array", () => {
+    expect(
+      mergeRanges([
+        { startTime: 0, endTime: 1 },
+        { startTime: 3, endTime: 5 },
+        { startTime: 4, endTime: 8 },
+        { startTime: 10, endTime: 12 },
+        { startTime: 9, endTime: 10 },
+      ])
+    ).toStrictEqual([
+      { startTime: 0, endTime: 1 },
+      { startTime: 3, endTime: 8 },
+      { startTime: 9, endTime: 12 },
+    ]);
+  });
+});
